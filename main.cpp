baf909f11133c5ce4e5ca47ec2111d187505cb12
pcismyname <pcismyname007@gmail.com>
2021-08-08 19:57:47 +0700

    ADD SOUND

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "Game.h"
+#include<SFML/Audio.hpp>
 
 int main()
 {
-	Game snakeGame;
+    sf::SoundBuffer soundBuffer;
+    if (!soundBuffer.loadFromFile("BGM\\sound.wav")) return -1;
+    sf::Sound sound;
+    sound.setBuffer(soundBuffer);
+    sound.play();
 
+    Game snakeGame;
 	snakeGame.start();
 
 	return 0;
